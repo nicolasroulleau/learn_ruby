17fa042acd0ce6d9eaa14b74a9345cde47b78b84
Nicolas Roulleau <nicolas.roulleau@gmail.com>
2018-01-16 23:47:29 +0100

    Exos 00,01,02,03 with bonus, 04

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -12,4 +12,22 @@ def sum(array)
   	array.inject(0, :+)
 end
 
+def multiply(*numbers)
+  result = 1
+  numbers.each { |n| result = result * n }
+  result
+end
+
+# numbers.reduce { |n, result| n * result }
+# or even numbers.reduce(:*)
+
+def power(a,b)
+	c= a**b
+	c
+end
+
+def factorial(n)
+	(1..n).inject(:*) || 1
+end
+
 
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
-#write your code here
+def translate(str)
+ i = 0
+
+ array = str.split.map! { |word|
+ 
+# On itère sur les lettres
+       
+       while word[i] =~ /[^[aeiou]]/ do
+         word << word[i]
+         word[0] = ""  
+       end
+
+     i += 1  
+     word << "ay"
+}
+
+result = array.join(" ")
+
+end
